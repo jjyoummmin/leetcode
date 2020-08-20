e7e0c957ebbb74c07f33ebb23cf2690a75322f7b
jjyoummmin <33231313+jjyoummmin@users.noreply.github.com>
2020-08-20 16:47:16 +0900

    Update 141.c
    add description

--- 141.c
+++ 141.c
@@ -1,6 +1,10 @@
-//link visited node to a trap node.
-//if there is no cycle, the traverse of linked list will end
-//else, fall into trap node
+/*
+problem => Given a linked list, determine if it has a cycle in it.
+link visited node to a trap node.
+if there is no cycle, the traverse of linked list will end
+else, fall into trap node
+*/
+
 
 /**
  * Definition for singly-linked list.
