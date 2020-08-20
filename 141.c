2bbf297f41813f2507ff008ffbe90b3aba13041f
jjyoummmin <33231313+jjyoummmin@users.noreply.github.com>
2020-08-20 16:47:48 +0900

    Update 141.c

--- 141.c
+++ 141.c
@@ -1,5 +1,6 @@
 /*
 problem => Given a linked list, determine if it has a cycle in it.
+solution =>
 link visited node to a trap node.
 if there is no cycle, the traverse of linked list will end
 else, fall into trap node
